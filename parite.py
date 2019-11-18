1e8c2c5aee65a7037b7d3ea81b8746b6ca2f7e5d
atastet <anthony@tastet.me>
2019-11-18 10:26:26 +0100

    [V0.2] Cleaning with good practices

---
+++ parite.py
@@ -0,0 +1,11 @@
+#! /usr/bin/env python3
+# coding: utf-8
+
+def main():
+    pass
+
+# Si exectuer en temps que programme principal X sinon Y 
+if __name__=="__main__":
+    main()
+else :
+    print("Parite is import as module")
